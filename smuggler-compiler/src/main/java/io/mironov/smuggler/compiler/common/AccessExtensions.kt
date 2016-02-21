6b7d9cb6ccda23130dafad2a5922e07540ac66f4
Vladimir Mironov <nsk.mironov@gmail.com>
2016-02-22 02:35:42 +0300

    More AccessExtensions

--- smuggler-compiler/src/main/java/io/mironov/smuggler/compiler/common/AccessExtensions.kt
+++ smuggler-compiler/src/main/java/io/mironov/smuggler/compiler/common/AccessExtensions.kt
@@ -104,3 +104,28 @@ internal val FieldSpec.isSynthetic: Boolean
 
 internal val FieldSpec.isStatic: Boolean
   get() = access and Opcodes.ACC_STATIC != 0
+
+
+internal val Int.isPublic: Boolean
+  get() = this and Opcodes.ACC_PUBLIC != 0
+
+internal val Int.isPrivate: Boolean
+  get() = this and Opcodes.ACC_PRIVATE != 0
+
+internal val Int.isProtected: Boolean
+  get() = this and Opcodes.ACC_PROTECTED != 0
+
+internal val Int.isInterface: Boolean
+  get() = this and Opcodes.ACC_INTERFACE != 0
+
+internal val Int.isAnnotation: Boolean
+  get() = this and Opcodes.ACC_ANNOTATION != 0
+
+internal val Int.isAbstract: Boolean
+  get() = this and Opcodes.ACC_ABSTRACT != 0
+
+internal val Int.isSynthetic: Boolean
+  get() = this and Opcodes.ACC_SYNTHETIC != 0
+
+internal val Int.isStatic: Boolean
+  get() = this and Opcodes.ACC_STATIC != 0
