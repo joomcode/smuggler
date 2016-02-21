3bf154f3dde3f632d922084ff0a125e36a4aa797
Vladimir Mironov <nsk.mironov@gmail.com>
2016-02-21 03:22:39 +0300

    Don't crash the app

--- smuggler-compiler/src/main/java/io/mironov/smuggler/compiler/SmugglerCompiler.kt
+++ smuggler-compiler/src/main/java/io/mironov/smuggler/compiler/SmugglerCompiler.kt
@@ -1,7 +1,11 @@
 package io.mironov.smuggler.compiler
 
+import org.apache.commons.io.FileUtils
+
 class SmugglerCompiler {
   fun compile(options: SmugglerOptions) {
-
+    options.inputs.forEach {
+      FileUtils.copyDirectory(it, options.output)
+    }
   }
 }
