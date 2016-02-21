64731c4c919f4eebcf33c38c00d9885496b34c17
Vladimir Mironov <nsk.mironov@gmail.com>
2016-02-21 14:53:14 +0300

    Add simple parcelable implementation for reference

--- smuggler-sample/src/main/java/io/mironov/smuggler/sample/SampleActivity.kt
+++ smuggler-sample/src/main/java/io/mironov/smuggler/sample/SampleActivity.kt
@@ -2,6 +2,8 @@ package io.mironov.smuggler.sample
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import io.mironov.smuggler.AutoParcelable
 
 class SampleActivity : Activity() {
@@ -11,6 +13,29 @@ class SampleActivity : Activity() {
   }
 }
 
-data class User(val firstName: String, val userName: String) : AutoParcelable
+data class User(val firstName: String, val lastName: String) : AutoParcelable {
+  companion object CREATOR : Parcelable.Creator<User> {
+    override fun newArray(size: Int): Array<out User?> {
+      return arrayOfNulls(size)
+    }
+
+    override fun createFromParcel(parcel: Parcel): User {
+      val firstName = parcel.readString()
+      val lastName = parcel.readString()
+
+      return User(firstName, lastName)
+    }
+  }
+
+  override fun writeToParcel(parcel: Parcel, flags: Int) {
+    parcel.writeString(firstName)
+    parcel.writeString(lastName)
+  }
+
+  override fun describeContents(): Int {
+    return 0
+  }
+}
+
 data class Message(val text: String, val user: User) : AutoParcelable
 data class Chat(val title: String, val messages: List<Message>, val participants: List<User>) : AutoParcelable
