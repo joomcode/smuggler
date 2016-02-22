ae24cf2f91b1d65d0cc283843196b5ffb4cb1200
Vladimir Mironov <nsk.mironov@gmail.com>
2016-02-22 19:35:52 +0300

    Get rid of data classes in SampleActivity

--- smuggler-sample/src/main/java/io/mironov/smuggler/sample/SampleActivity.kt
+++ smuggler-sample/src/main/java/io/mironov/smuggler/sample/SampleActivity.kt
@@ -2,7 +2,6 @@ package io.mironov.smuggler.sample
 
 import android.app.Activity
 import android.os.Bundle
-import io.mironov.smuggler.AutoParcelable
 
 class SampleActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,26 +9,3 @@ class SampleActivity : Activity() {
     setContentView(R.layout.activity_sample)
   }
 }
-
-data class User(
-    val firstName: String,
-    val lastName: String
-) : AutoParcelable
-
-data class Message(
-    val id: Int,
-    val sender: User,
-    val text: String,
-    val timestamp: Long,
-    val magic: Short,
-    val seen: Boolean,
-    val extras: Bundle,
-
-    val booleans: BooleanArray,
-    val chars: CharArray,
-    val doubles: DoubleArray,
-    val floats: FloatArray,
-    val ints: IntArray,
-    val logns: LongArray,
-    val strings: Array<String>
-) : AutoParcelable
