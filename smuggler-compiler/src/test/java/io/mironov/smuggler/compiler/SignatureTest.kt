6ae9104f3bf4d8898950fd5a21ae810a7c030b10
Vladimir Mironov <nsk.mironov@gmail.com>
2016-03-02 15:28:40 +0300

    Setup tests for compiler

---
+++ smuggler-compiler/src/test/java/io/mironov/smuggler/compiler/SignatureTest.kt
@@ -0,0 +1,13 @@
+package io.mironov.smuggler.compiler
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class SignatureTest {
+  @Test fun shouldFail() {
+    Assert.assertEquals(true, false)
+  }
+}
