97591a70e24af76df25efad5193b60b5889fea1c
Vladimir Mironov <nsk.mironov@gmail.com>
2016-03-07 02:05:13 +0300

    Get rid of useless constants

--- smuggler-compiler/src/main/java/io/mironov/smuggler/compiler/annotations/Metadata.kt
+++ smuggler-compiler/src/main/java/io/mironov/smuggler/compiler/annotations/Metadata.kt
@@ -2,14 +2,6 @@ package io.mironov.smuggler.compiler.annotations
 
 @AnnotationDelegate("kotlin.Metadata")
 interface Metadata {
-  companion object {
-    const val KIND_CLASS = 1
-    const val KIND_FILE = 2
-    const val KIND_SYNTHETIC_CLASS = 3
-    const val KIND_MULTIFILE_CLASS_FACADE = 4
-    const val KIND_MULTIFILE_CLASS_PART = 5
-  }
-
   fun k(): Int
 
   fun d1(): Array<String>
