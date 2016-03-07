de6672f994404107abbdca195a2af2001626648e
Vladimir Mironov <nsk.mironov@gmail.com>
2016-03-07 03:36:40 +0300

    Properly invoke TypeAdapter.fromParcel

--- smuggler-compiler/src/main/java/io/mironov/smuggler/compiler/generators/ValueAdapter.kt
+++ smuggler-compiler/src/main/java/io/mironov/smuggler/compiler/generators/ValueAdapter.kt
@@ -569,21 +569,21 @@ internal class AdaptedValueAdapter(
 ) : OptionalValueAdapter() {
   private companion object {
     private val METHOD_TO_PARCEL = Methods.get("toParcel", Types.VOID, Types.OBJECT, Types.ANDROID_PARCEL, Types.INT)
-    private val METHOD_FROM_PARCEL = Methods.get("fromParcel", Types.OBJECT)
+    private val METHOD_FROM_PARCEL = Methods.get("fromParcel", Types.OBJECT, Types.ANDROID_PARCEL)
   }
 
   override fun fromParcelNotNull(adapter: GeneratorAdapter, context: ValueContext) {
     adapter.newInstance(adapterType, Methods.getConstructor())
-    adapter.loadArg(context.parcel())
+    adapter.loadLocal(context.parcel())
     adapter.invokeInterface(Types.SMUGGLER_ADAPTER, METHOD_FROM_PARCEL)
     adapter.checkCast(elementType)
   }
 
   override fun toParcelNotNull(adapter: GeneratorAdapter, context: ValueContext) {
     adapter.newInstance(adapterType, Methods.getConstructor())
-    adapter.loadArg(context.value())
-    adapter.loadArg(context.parcel())
-    adapter.loadArg(context.flags())
+    adapter.loadLocal(context.value())
+    adapter.loadLocal(context.parcel())
+    adapter.loadLocal(context.flags())
     adapter.invokeInterface(Types.SMUGGLER_ADAPTER, METHOD_TO_PARCEL)
   }
 }
