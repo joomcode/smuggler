cc9f7c45f51ee23e2534018bb19c26c05ae2e162
Vladimir Mironov <nsk.mironov@gmail.com>
2019-04-13 18:29:40 +0300

    Fix some deprecation warnings.

--- smuggler-compiler/src/main/java/com/joom/smuggler/compiler/common/Types.kt
+++ smuggler-compiler/src/main/java/com/joom/smuggler/compiler/common/Types.kt
@@ -4,17 +4,12 @@ import com.joom.smuggler.compiler.annotations.AnnotationDelegate
 import org.objectweb.asm.Type
 import java.io.Serializable
 import java.util.ArrayList
-import java.util.Collection
 import java.util.Date
 import java.util.HashMap
 import java.util.HashSet
-import java.util.Iterator
 import java.util.LinkedHashMap
 import java.util.LinkedHashSet
 import java.util.LinkedList
-import java.util.List
-import java.util.Map
-import java.util.Set
 import java.util.SortedMap
 import java.util.SortedSet
 import java.util.TreeMap
